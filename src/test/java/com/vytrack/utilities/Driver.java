a2d505eabcdc9f27ec33e73d58ef921c80e45064
Vali Tugui <vali.tuguii@gmail.com>
2022-02-17 18:00:47 +0000

    day7: paralel pow adjusted

--- src/test/java/com/vytrack/utilities/Driver.java
+++ src/test/java/com/vytrack/utilities/Driver.java
@@ -69,7 +69,7 @@ public class Driver {
                     ChromeOptions chromeOptions = new ChromeOptions();
                     chromeOptions.setCapability("platform", Platform.ANY);
                     try {
-                        driverPool.set(new RemoteWebDriver(new URL("http://44.202.12.174/:5555/wd/hub"),chromeOptions));
+                        driverPool.set(new RemoteWebDriver(new URL("http://44.202.12.174:5555/wd/hub"),chromeOptions));
                     } catch (MalformedURLException e) {
                         e.printStackTrace();
                     }
